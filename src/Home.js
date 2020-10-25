2754b3806f1d9b5ecd9f57db55e9ce52899e739a
Byron Lutz <byronlutz@gmail.com>
2020-10-24 22:41:33 -0700

    Added slide links to the homepage

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
 function Home() {
-  return <div>Homepage</div>
+  return <div>
+    <h2>Slides</h2>
+    <p>Navigate the non-Google slides with the arrows in the corner or the arrow keys on your keyboard</p>
+    <ul>
+      <li><a href='/slides/intro.html'>Intro/Journalism</a></li>
+      <li><a href='/slides/html.html'>HTML</a></li>
+      <li><a href='/slides/css.html'>CSS</a></li>
+    </ul>
+  </div>
 }
 
 export default Home
