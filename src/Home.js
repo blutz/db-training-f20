24304f1a52c79c302e6e00096105146d47931638
Byron Lutz <byronlutz@gmail.com>
2020-10-24 23:38:20 -0700

    Added git presentation

--- src/Home.js
+++ src/Home.js
@@ -8,6 +8,7 @@ function Home() {
       <li><a href='/slides/intro.html'>Intro/Journalism</a></li>
       <li><a href='/slides/html.html'>HTML</a></li>
       <li><a href='/slides/css.html'>CSS</a></li>
+      <li><a href='https://docs.google.com/presentation/d/1sjXNHiMxkigvD12SvLz6XNOe89u5Y4BbFOCHJoAaK_M/edit?usp=sharing'>Git</a></li>
     </ul>
   </div>
 }
